43aa5879c28bcb92a2566c3bdec928ff9972ef52
mashimine <mauricio@caylent.com>
2021-08-12 14:44:47 -0300

    add ssm creation in bootstap script

--- src/resources/local-stack/bin/bootstrap.sh
+++ src/resources/local-stack/bin/bootstrap.sh
@@ -2,6 +2,21 @@
 
 set -e
 
+SLS_DEPLOYMENT_BUCKET_SSM_NAME="s3-serverless-deployment"
+S3_BUCKET_NAME="local-marley-sls-deployments"
+ENDPOINT_URL="http://${LOCALSTACK_HOSTNAME}:4566"
+
 echo "Attempting to create deployment bucket..."
-aws s3 mb s3://local-marley-sls-deployments --endpoint-url http://${LOCALSTACK_HOSTNAME}:4566
+aws s3 mb s3://${S3_BUCKET_NAME} \
+--endpoint-url $ENDPOINT_URL
 echo "Done"
+
+echo "Attempting to create SSM parameters..."
+aws ssm put-parameter \
+--name "${SLS_DEPLOYMENT_BUCKET_SSM_NAME}" \
+--type "String" \
+--value "$S3_BUCKET_NAME" \
+--endpoint-url $ENDPOINT_URL \
+--overwrite
+echo "Done"
+
